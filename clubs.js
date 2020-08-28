0a8188910f7eef0dcfad0ad5aed378140fc6243a
Cedar1000 <47836121+Cedar1000@users.noreply.github.com>
2020-08-29 00:57:29 +0100

    Create clubs.js

---
+++ clubs.js
@@ -0,0 +1,98 @@
+let clubs = [
+  {
+    name: "Chelsea",
+    country: "England",
+    group: 1,
+    matched: false,
+  },
+  {
+    name: "Psg",
+    country: "France",
+    group: 1,
+    matched: false,
+  },
+  {
+    name: "Real Madrid",
+    country: "Spain",
+    group: 2,
+    matched: false,
+  },
+  {
+    name: "Man  City",
+    country: "England",
+    group: 2,
+    matched: false,
+  },
+  {
+    name: "Barcelona",
+    country: "Spain",
+    group: 3,
+    matched: false,
+  },
+  {
+    name: "Juventus",
+    country: "Italy",
+    group: 3,
+    matched: false,
+  },
+  {
+    name: "Bayern Munchen",
+    country: "Germany",
+    group: 4,
+    matched: false,
+  },
+  {
+    name: "Monaco",
+    country: "France",
+    group: 4,
+    matched: false,
+  },
+  {
+    name: "Dortmund",
+    country: "Germany",
+    group: 5,
+    matched: false,
+  },
+  {
+    name: "Inter",
+    country: "Italy",
+    group: 5,
+    matched: false,
+  },
+  {
+    name: "Ajax",
+    country: "Netherlands",
+    group: 6,
+    matched: false,
+  },
+  {
+    name: "Atletico Madrid",
+    country: "Spain",
+    group: 6,
+    matched: false,
+  },
+  {
+    name: "Man United",
+    country: "England",
+    group: 7,
+    matched: false,
+  },
+  {
+    name: "Olympiacos",
+    country: "Netherlands",
+    group: 7,
+    matched: false,
+  },
+  {
+    name: "Lyon",
+    country: "France",
+    group: 8,
+    matched: false,
+  },
+  {
+    name: "Bayer Leverkusen",
+    country: "Germany",
+    group: 8,
+    matched: false,
+  },
+];
